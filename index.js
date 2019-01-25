aa26efcf9f57e75cc7bf9f20a675a7c4cb2e9ce0
Yvan Tchoffo <yvan.tchoffo@yahoo.com>
2019-01-25 03:15:58 +0100

    Fix majeur

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var db = require('./db/db.js');
 var session = require('express-session');
 var login = require('./login.js');
 var signup = require('./signup.js');
+var logout = require('./logout.js');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var upload = multer();
@@ -12,9 +13,16 @@ app.set('view engine', 'pug');
 app.set('views','./views');
 app.use(express.static('public'));
 app.use('/scripts', express.static('node_modules'));
-app.use(session({secret: "@easyumlsessionkey"}));
+app.use(session({
+    name:"cookie",
+    secret: "@easyumlsessionkey",
+    resave: true,
+    saveUninitialized: true,
+
+}));
 app.use('/login',login);
 app.use('/signup',signup);
+app.use('/logout',logout);
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(upload.array()); 
@@ -30,7 +38,7 @@ function checkSignIn(req, res,next){
 
 app.get('/',checkSignIn,function(req,res,next){
     data = db.query("select * from diagramme where user_id = ?",[req.session.user.id],function(err,rows,fields){
-        res.render('app/dashboard',{data: rows});
+        res.render('app/dashboard',{data: rows, user:req.session.user});
     });
     
 });
@@ -41,28 +49,32 @@ app.get('/signup',function(req,res,next){
 
 app.post('/save',checkSignIn,function(req,res,next){
     id = req.session.user.id;
-    console.log(id);
     if(req.body.newdia != -1){
         db.query("update diagramme set content = ? where id = ? ",[req.body.data,req.session.diagramme],function(err,result,next){
+            if(err)
+                res.send(err);
             res.send("ok");
         })
     }else{
-        db.query("select * from diagramme where nom = ?",[req.body.name],function(err,rows,next){
-            if(rows.length)
+        db.query("select * from diagramme where nom = ? and user_id = ?",[req.body.name,id],function(err,rows,next){
+            if(rows.length){
                 res.send("double");
-        });
-        db.query("insert into diagramme(nom,date,user_id,content) values (?,now(),?,?)",[req.body.name,id,req.body.data],function(err,result,next){
-            if(err)
-                res.send(err);
-            else
-                // req.session.diagramme = 
-                res.send("ok");
+            }
+            else{
+                db.query("insert into diagramme(nom,date,user_id,content) values (?,now(),?,?)",[req.body.name,id,req.body.data],function(err,result,next){
+                    if(err)
+                        res.send(err);
+                    else
+                        res.send({id :result.insertId});
+                });
+            }
+                
         });
     }
 })
 
 app.get('/add',checkSignIn,function(req,res,next){
-    res.render("app/paperwork",{newdia:-1,data:-1});
+    res.render("app/paperwork",{newdia:-1,data:-1, user:req.session.user});
 });
 
 app.post('/delete',checkSignIn,function(req,res,next){
@@ -77,9 +89,9 @@ app.post('/delete',checkSignIn,function(req,res,next){
 app.get('/open/:id',checkSignIn,function(req,res,next){
     db.query("select * from diagramme where id=?",[req.params.id],function(err,rows,fields){
         req.session.diagramme = req.params.id;
-        res.render("app/paperwork",{newdia:0, data:JSON.stringify(rows[0].content)});
+        res.render("app/paperwork",{newdia:0, data:JSON.stringify(rows[0].content), user:req.session.user});
     });
-})
+});
 
 
 app.listen(3000);
